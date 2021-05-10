046cb01674019fc31c412ac0245532c4baea67ff
AshleyThew <ashl.e.thew@gmail.com>
2021-05-11 04:33:12 +1200

    Comment & Javadocs
    Start work on Comments & Javadocs

--- src/main/java/me/dablakbandit/bank/BankCoreHandler.java
+++ src/main/java/me/dablakbandit/bank/BankCoreHandler.java
@@ -23,10 +23,19 @@ import me.dablakbandit.core.players.CorePlayerManager;
 import me.dablakbandit.core.plugin.CoreHandler;
 import me.dablakbandit.core.updater.PluginUpdater;
 
+/**
+ * The Bank core handler.
+ * Replacement of Bank plugin main package, used to load, enable and disable plugin elements.
+ */
 public class BankCoreHandler extends CoreHandler implements Listener{
 	
 	private static BankCoreHandler main = new BankCoreHandler();
 	
+	/**
+	 * Get bank core handler instance.
+	 *
+	 * @return the bank core handler
+	 */
 	public static BankCoreHandler getInstance(){
 		return main;
 	}
@@ -36,51 +45,106 @@ public class BankCoreHandler extends CoreHandler implements Listener{
 	private boolean			upgraded		= false;
 	private boolean			firstInstall	= false;
 	
+	/**
+	 * Handler load.
+	 */
 	public void onLoad(){
+		// Assign plugin instance for reference
 		plugin = BankPlugin.getInstance();
+		
+		// Check for plugin updates from spigot
 		PluginUpdater.getInstance().checkUpdate(plugin, "18968");
+		// Check if an upgrade exists
 		upgraded = checkUpgrade();
+		// Check for plugin first install
 		checkFirstInstall();
+		
+		// Load elements of the plugin
 		load();
+		
+		// Register BankPlayerManager with core plugin
 		CorePlayerManager.getInstance().addListener(BankPlayerManager.getInstance());
 	}
 	
+	/**
+	 * Check if this is the plugins first run time.
+	 */
 	private void checkFirstInstall(){
 		firstInstall = !new File(plugin.getDataFolder(), "config.yml").exists();
 	}
 	
+	/**
+	 * Handler load elements of the plugin.
+	 */
 	private void load(){
+		// Initialize loader thread
 		LoaderManager.getInstance();
+		
+		// Load elements of plugin
 		loadConfigurations();
 		loadDatabase();
 		loadCommands();
 		loadOther();
+		
+		// Notify console bank has loaded
 		BankLog.info(BankPluginConfiguration.BANK_LOG_PLUGIN_LEVEL, "Bank loaded");
 	}
 	
+	/**
+	 * Reload elements of the plugin.
+	 * Calls methods in order, disable, load, enable.
+	 * @see #disable() 
+	 * @see #load() 
+	 * @see #enable() 
+	 */
 	public void reload(){
 		disable();
 		load();
 		enable();
 	}
 	
+	/**
+	 * Checks the upgrade from bank versions below 4.1.0 for backwards compatibility.
+	 * @see UpgradeManager
+	 */
 	private boolean checkUpgrade(){
+		// Check for upgrade
 		if(upgradeManager.hasUpgrade()){
+			// Ensure owner has confirmed the upgrade process
 			if(!upgradeManager.confirmUpgrade()){
+				// Ask owner to confirm upgrade
 				BankUpgradeConfiguration.getInstance().notifyUpgrade();
 				upgradeManager.printUpgradeAndShutdown();
 				return false;
 			}
+			// Perform upgrade
 			upgradeManager.upgrade();
 			BankUpgradeConfiguration.getInstance().saveConfig();
 		}
 		return true;
 	}
 	
+	/**
+	 * Loads database with SaveType from config.
+	 * @see me.dablakbandit.bank.save.type.SaveType
+	 * @see BankDatabaseManager
+	 * @see BankPluginConfiguration
+	 */
 	private void loadDatabase(){
 		BankDatabaseManager.getInstance().load(BankPluginConfiguration.BANK_SAVE_TYPE.get());
 	}
 	
+	/**
+	 * Loads all configuration elements for the plugin.
+	 * @see BankPluginConfiguration
+	 * @see BankLanguageConfiguration
+	 * @see BankCommandConfiguration
+	 * @see BankItemConfiguration
+	 * @see BankInventoryConfiguration
+	 * @see BankPermissionConfiguration
+	 * @see BankItemBlacklistConfiguration
+	 * @see BankSoundConfiguration
+	 */
 	private void loadConfigurations(){
 		BankPluginConfiguration.getInstance().load();
 		BankLanguageConfiguration.getInstance().load();
@@ -92,69 +156,133 @@ public class BankCoreHandler extends CoreHandler implements Listener{
 		BankSoundConfiguration.getInstance().load();
 	}
 	
+	/**
+	 * Load bank command for the plugin.
+	 */
 	private void loadCommands(){
 		BankCommand.getInstance().load();
 	}
 	
+	/**
+	 * Loads other elements of the plugin, to be called last.
+	 * Inventories, Implementations, Expire & Converters.
+	 * @see BankInventoriesManager
+	 * @see BankImplementationManager
+	 * @see Converters
+	 */
 	private void loadOther(){
 		BankInventoriesManager.getInstance().load();
 		BankImplementationManager.getInstance().load();
 		Converters.load();
 	}
 	
+	/**
+	 * Handler enable.
+	 */
 	public void onEnable(){
+		// If the upgrade process failed don't continue to enable
 		if(!upgraded){ return; }
 		
+		// Handle first install
 		if(firstInstall){
 			handleFirstInstall();
 		}
 		
+		// Enable elements of plugin
 		enable();
+		
+		// Start loader thread
 		LoaderManager.getInstance().start();
+		// Report metrics
 		addMetrics();
+		// Enable Bank Player Manager
 		BankPlayerManager.getInstance().enable();
+		// Initialize placeholders
 		BankPlaceholderManager.getInstance();
 	}
 	
-	public void enable(){
+	/**
+	 * Enable elements of the plugin.
+	 */
+	private void enable(){
+		// Enable implementations
 		BankImplementationManager.getInstance().enable();
+		// Enable autosave
 		AutoSaveManager.getInstance().enable();
+		
+		// Notify console bank has enabled
 		BankLog.info(BankPluginConfiguration.BANK_LOG_PLUGIN_LEVEL, "Bank Enabled");
 	}
 	
-	public void disable(){
+	/**
+	 * Disable elements of the plugin.
+	 */
+	private void disable(){
+		// Remove plugin commands
 		AbstractCommand.removePluginCommands(plugin);
+		
+		// Disable all implementations
 		BankImplementationManager.getInstance().disable();
+		// Disable autosave
 		AutoSaveManager.getInstance().disable();
+		// Disable converters
 		Converters.disable();
+		
+		// Notify console bank has disabled
 		BankLog.info(BankPluginConfiguration.BANK_LOG_PLUGIN_LEVEL, "Bank Disabled");
 	}
 	
+	/**
+	 * Handler disable.
+	 */
 	public void onDisable(){
+		// If plugin failed to upgrade
 		if(!upgraded){ return; }
+		
+		// Disable elements of the plugin
 		disable();
+		
+		//
 		BankPlayerManager.getInstance().disable();
 		LoaderManager.getInstance().stop();
 	}
 	
+	/**
+	 * Enable bStats metrics.
+	 */
 	private void addMetrics(){
 		try{
+			// Start metrics with plugin instance
 			Metrics m = new Metrics(plugin);
+			// Add custom chart for savetype
 			m.addCustomChart(new Metrics.SimplePie("save_type", () -> BankPluginConfiguration.BANK_SAVE_TYPE.get().name()));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Check for elements when first installed.
+	 * @see #checkCitizens() 
+	 */
 	private void handleFirstInstall(){
 		firstInstall = false;
+		// Notify console of first install
 		BankLog.info(BankPluginConfiguration.BANK_LOG_PLUGIN_LEVEL, "First install, detecting other plugins.");
+		
+		// Checks
 		checkCitizens();
 	}
 	
+	/**
+	 * On first install check for citizens plugin to enable
+	 */
 	private void checkCitizens(){
+		// If the Citizens plugin exists
 		if(Bukkit.getPluginManager().getPlugin("Citizens") != null){
+			// Notify console of citizens detection
 			BankLog.info(BankPluginConfiguration.BANK_LOG_PLUGIN_LEVEL, "Detected citizens enabling.");
+			// Enable citizens support
 			BankPluginConfiguration.BANK_TYPE_CITIZENS_ENABLED.set(true);
 		}
 	}
--- src/main/java/me/dablakbandit/bank/BankPlugin.java
+++ src/main/java/me/dablakbandit/bank/BankPlugin.java
@@ -9,30 +9,56 @@ import org.bukkit.plugin.java.JavaPlugin;
 import me.dablakbandit.bank.log.BankLog;
 import me.dablakbandit.core.plugin.downloader.CorePluginDownloader;
 
+/**
+ * The entry point to the bank plugin.
+ * Ensures core plugin is downloaded before continuing,
+ * Else errors print to console.
+ * @see me.dablakbandit.core.plugin.downloader.CorePluginDownloader
+ */
 public class BankPlugin extends JavaPlugin{
 	
 	private static BankPlugin	main;
 	private BankCoreHandler		handler;
 	
+	/**
+	 * Get bank plugin instance.
+	 *
+	 * @return the bank plugin
+	 */
 	public static BankPlugin getInstance(){
 		return main;
 	}
 	
+	/**
+	 * Plugin load.
+	 */
 	public void onLoad(){
+		// Assign instance
 		main = this;
+		
+		// Check if the core plugin exists
 		if(CorePluginDownloader.ensureCorePlugin()){
+			// Assign and load handler
 			handler = BankCoreHandler.getInstance();
 			handler.onLoad();
 		}
 	}
 	
+	/**
+	 * Plugin enabling.
+	 */
 	public void onEnable(){
+		// Enable handler if assigned
 		if(handler != null){
 			handler.onEnable();
 		}
 	}
 	
+	/**
+	 * Plugin disabling.
+	 */
 	public void onDisable(){
+		// Disable handler if assigned else print error
 		if(handler != null){
 			handler.onDisable();
 		}else{
@@ -40,6 +66,9 @@ public class BankPlugin extends JavaPlugin{
 		}
 	}
 	
+	/**
+	 * Print error to console when core plugin is missing.
+	 */
 	private void printError(){
 		for(int i = 0; i < 5; i++){
 			BankLog.errorAlways("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
